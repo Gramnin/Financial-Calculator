3bac3c1ad10f51ebe3b06d1ca3e793773189a1ea
Brian <brjhodges@gmail.com>
2016-10-20 17:23:55 -0500

    Converted value.py to use menu module

--- value.py
+++ value.py
@@ -17,32 +17,17 @@ def fv(payment: float, rate: float, periods: float) -> float:
     factor = numerator / rate
     return payment * factor
 
+def get_value(func):
+    payment = input("Payment value: ")
+    rate = input("Interest rate: ")
+    periods = input("Number of periods: ")
+    out = func(payment, rate, periods)
+    print("%.4f" % out)
+
 if __name__ == '__main__':
-    import sys
+    from menu_utils import menu_loop
     
-    type = None
-    if len(sys.argv) > 1:
-        type = sys.argv[1]
-    eq = {
-        'pv': pv,
-        'fv': fv,
-    }
-    while type not in eq:
-        try:
-            type = input('Type (%s): ' % ', '.join(sorted(eq.keys())))
-        except KeyboardInterrupt:
-            print()
-            sys.exit(0)
-    eq = eq[type]
-    if len(sys.argv) >= 5:
-        payment, rate, periods = sys.argv[2:5]
-    else:
-        try:
-            payment = input('Payment: ')
-            rate = input('Rate: ')
-            periods = input('Periods: ')
-        except KeyboardInterrupt:
-            print()
-            sys.exit(0)
-    
-    print(eq(payment, rate, periods))
+    menu_loop([
+        (1, "Present Value", get_value, pv),
+        (2, "Future Value", get_value, fv),
+    ])
